f1f3ef4d3031b00fd0857b5369a3d1503a542ab2
chenjiandongx <chenjiandongx@qq.com>
2017-12-16 16:46:32 +0800

    邮件内容完善

--- subscribe.py
+++ subscribe.py
@@ -13,9 +13,9 @@ ZHAOQING = "肇庆"
 MAIL_HOST = os.environ.get('MAIL_HOST')
 MAIL_USER = os.environ.get('MAIL_USER')
 MAIL_PASS = os.environ.get('MAIL_PASS')
-RECEIVERS = [os.environ.get('RECEIVER')]
 WEATHER_KEY = os.environ.get('WEATHER_KEY')
 
+RECEIVERS = [].append(os.environ.get('RECEIVER'))
 SENDER = 'chenjiandongx@qq.com'
 
 
@@ -32,13 +32,22 @@ def get_weather_info():
         weather_api.format(cityname=GUANGZHOU, key=WEATHER_KEY)).json()
 
     today = req['result']['today']
+    if today:
 
-    content = "温度：{temperature}，天气为：{weather}".format(
-        temperature=today['temperature'],
-        weather=today['weather']
-    )
-    print("do0")
-    return content
+        content = "傻宝宝你好:\n\n  " \
+                  "今天是{date_y}，{week},是我们相恋的第{loving_days}天，" \
+                  "广州今天{weather}，{wind}，气温{temperature}，" \
+                  "感觉{dressing_index}，{dressing_advice}。"
+        return content.format(
+            date_y=today['date_y'],
+            week=today['week'],
+            loving_day=get_loving_days(),
+            weather=today['weather'],
+            wind=today['wind'],
+            temperature=today['temperature'],
+            dressing_index=today['dressing_index'],
+            dressing_advice=today['dressing_advice']
+        )
 
 
 def get_loving_days():
@@ -51,24 +60,24 @@ def get_loving_days():
     return (today - anniversary).days
 
 
-def send_email(content):
+def send_email():
     """
 
     :return:
     """
+    content = get_weather_info()
     message = MIMEText(content, 'plain', 'utf-8')
-    message['From'] = Header('A handsome boy', 'utf-8')
-    message['To'] = Header('A beautiful girl')
-    message['Subject'] = Header('日常关心', 'utf-8')
+    message['From'] = Header('暖宝宝', 'utf-8')
+    message['To'] = Header('傻宝宝')
+    message['Subject'] = Header('来自暖宝宝的日常问候', 'utf-8')
     try:
         smtpObj = smtplib.SMTP_SSL(MAIL_HOST)
         smtpObj.login(MAIL_USER, MAIL_PASS)
-        smtpObj.sendmail(SENDER, ['13025332131@163.com'], message.as_string())
+        smtpObj.sendmail(SENDER, RECEIVERS, message.as_string())
         smtpObj.quit()
     except Exception as e:
         print(e)
 
 
 if __name__ == "__main__":
-    content = get_weather_info()
-    send_email(content)
+    send_email()
