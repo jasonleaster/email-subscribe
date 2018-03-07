383c780eba66e9bd8b864394eafee0a81fcc72dd
chenjiandongx <chenjiandongx@qq.com>
2018-03-07 20:25:36 +0800

    确保状态正确才执行发送任务

--- subscribe.py
+++ subscribe.py
@@ -31,47 +31,49 @@ def get_weather_info():
     """ 获取天气信息
     """
     guangzhou = requests.get(weather_api.format(GUANGZHOU, headers=headers)).json()
-    guangzhou_today = guangzhou['data']['forecast'][0]
-    guangzhou_tomorrow=guangzhou['data']['forecast'][1]
-
     zhaoqing = requests.get(weather_api.format(ZHAOQING, headers=headers)).json()
-    zhaoqing_today = zhaoqing['data']['forecast'][0]
-    zhaoqing_tomorrow = zhaoqing['data']['forecast'][1]
 
-    if guangzhou and zhaoqing:
-        content = (
-            "你好，傻宝宝:\n\n\t"
-            "今天是 {_today}，{_week}。\n\t"
-            "首先，今天已经是我们相恋的第 {loving_days} 天了喔。然后我就要来播送天气预报了！！\n\n\t"
-            "广州今天{_g_today_high}，{_g_today_low}，天气 {_g_today_type}，"
-            "需要注意的是{_g_today_notice}\n\t"
-            "广州明天{_g_tomorrow_high}，{_g_tomorrow_low}，天气 {_g_tomorrow_type}，"
-            "需要注意的是{_g_tomorrow_notice}\n\n\t"
-            "肇庆今天{_z_today_high}，{_z_today_low}，天气 {_z_today_type}，"
-            "需要注意的是{_z_today_notice}\n\t"
-            "肇庆明天{_z_tomorrow_high}，{_z_tomorrow_low}，天气 {_z_tomorrow_type}，"
-            "需要注意的是{_z_tomorrow_notice}")
-        return content.format(
-            loving_days=get_loving_days(),
-            _today=get_today(guangzhou['date']),
-            _week=guangzhou_today['date'][-3:],
-            _g_today_high=guangzhou_today['high'],
-            _g_today_low=guangzhou_today['low'],
-            _g_today_type=guangzhou_today['type'],
-            _g_today_notice=guangzhou_today['notice'],
-            _g_tomorrow_high=guangzhou_tomorrow['high'],
-            _g_tomorrow_low=guangzhou_tomorrow['low'],
-            _g_tomorrow_type=guangzhou_tomorrow['type'],
-            _g_tomorrow_notice=guangzhou_tomorrow['notice'],
-            _z_today_high=zhaoqing_today['high'],
-            _z_today_low=zhaoqing_today['low'],
-            _z_today_type=zhaoqing_today['type'],
-            _z_today_notice=zhaoqing_today['notice'],
-            _z_tomorrow_high=zhaoqing_tomorrow['high'],
-            _z_tomorrow_low=zhaoqing_tomorrow['low'],
-            _z_tomorrow_type=zhaoqing_tomorrow['type'],
-            _z_tomorrow_notice=zhaoqing_tomorrow['notice'],
-        )
+    if guangzhou['status'] == 200 and zhaoqing['status'] == 200:
+        guangzhou_today = guangzhou['data']['forecast'][0]
+        guangzhou_tomorrow=guangzhou['data']['forecast'][1]
+
+        zhaoqing_today = zhaoqing['data']['forecast'][0]
+        zhaoqing_tomorrow = zhaoqing['data']['forecast'][1]
+
+        if guangzhou and zhaoqing:
+            content = (
+                "你好，傻宝宝:\n\n\t"
+                "今天是 {_today}，{_week}。\n\t"
+                "首先，今天已经是我们相恋的第 {loving_days} 天了喔。然后我就要来播送天气预报了！！\n\n\t"
+                "广州今天{_g_today_high}，{_g_today_low}，天气 {_g_today_type}，"
+                "需要注意的是{_g_today_notice}\n\t"
+                "广州明天{_g_tomorrow_high}，{_g_tomorrow_low}，天气 {_g_tomorrow_type}，"
+                "需要注意的是{_g_tomorrow_notice}\n\n\t"
+                "肇庆今天{_z_today_high}，{_z_today_low}，天气 {_z_today_type}，"
+                "需要注意的是{_z_today_notice}\n\t"
+                "肇庆明天{_z_tomorrow_high}，{_z_tomorrow_low}，天气 {_z_tomorrow_type}，"
+                "需要注意的是{_z_tomorrow_notice}")
+            return content.format(
+                loving_days=get_loving_days(),
+                _today=get_today(guangzhou['date']),
+                _week=guangzhou_today['date'][-3:],
+                _g_today_high=guangzhou_today['high'],
+                _g_today_low=guangzhou_today['low'],
+                _g_today_type=guangzhou_today['type'],
+                _g_today_notice=guangzhou_today['notice'],
+                _g_tomorrow_high=guangzhou_tomorrow['high'],
+                _g_tomorrow_low=guangzhou_tomorrow['low'],
+                _g_tomorrow_type=guangzhou_tomorrow['type'],
+                _g_tomorrow_notice=guangzhou_tomorrow['notice'],
+                _z_today_high=zhaoqing_today['high'],
+                _z_today_low=zhaoqing_today['low'],
+                _z_today_type=zhaoqing_today['type'],
+                _z_today_notice=zhaoqing_today['notice'],
+                _z_tomorrow_high=zhaoqing_tomorrow['high'],
+                _z_tomorrow_low=zhaoqing_tomorrow['low'],
+                _z_tomorrow_type=zhaoqing_tomorrow['type'],
+                _z_tomorrow_notice=zhaoqing_tomorrow['notice'],
+            )
 
 
 def get_loving_days():
@@ -104,4 +106,5 @@ def send_email():
 
 
 if __name__ == "__main__":
-    send_email()
+    print(get_weather_info())
+    # send_email()
